70d554aa8a5a217ac127bfaaacb26c8c0e91dc3f
taoliucheng <java_taolc@163.com>
2018-07-31 10:20:45 +0800

    update

---
+++ src/test/java/com/taolc/http/dwk_wechat/RedisConnectTest.java
@@ -0,0 +1,56 @@
+package com.taolc.http.dwk_wechat;
+
+import com.taolc.http.HttpClientTool;
+
+/**
+ * redis 连接测试
+ */
+public class RedisConnectTest {
+
+    public static void main(String[] args) {
+        devConnectTest();
+//        testConnectTest();
+//        prodQQConnectTest();
+//        prodWechatConnectTest();
+    }
+
+    /**
+     * 开发环境测试
+     */
+    public static void devConnectTest(){
+        String url = "http://localhost:8080";
+        http(url);
+    }
+
+    /**
+     * 测试环境测试
+     */
+    public static void testConnectTest(){
+        String url = "http://test.qquser.mur.qq.com";
+        http(url);
+    }
+
+    /**
+     * 微信正式环境测试
+     */
+    public static void prodWechatConnectTest(){
+        String url = "http://api.user.mur.qq.com";
+        http(url);
+    }
+
+    /**
+     * QQ正式环境测试
+     */
+    public static void prodQQConnectTest(){
+        String url = "http://api.qquser.mur.qq.com";
+        http(url);
+    }
+
+    private static void http(String url){
+        String putResponse = HttpClientTool.post(url + "/redis/put");
+        System.out.println("向redis放值 --> " + putResponse);
+
+        String getResponse = HttpClientTool.post(url + "/redis/get");
+        System.out.println("获取redis中的值 --> " + getResponse);
+    }
+}
